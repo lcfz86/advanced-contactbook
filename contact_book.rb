1be4bf32794c033fae3559d5f7e57848ae8c90dc
lcfz86 <zerolcfz86@gmail.com>
2021-06-17 20:52:01 +0800

    Add Contact Book Part 5

--- contact_book.rb
+++ contact_book.rb
@@ -1,3 +1,6 @@
+# Contact Book Part 5
+require './person'
+
 # Contact Book Part 2
 class ContactBook
   def initialize
@@ -8,6 +11,17 @@ class ContactBook
     @contact.count
   end
 
-  def add_contact
+# Contact Book Part 5
+  def add_contact(name, phone_numbers, emails)
+    new_contact = Contact.new(name)
+    phone_numbers.each do |phone_num|
+      new_contact.add_phone_number(phone_num)
+    end
+
+    emails.each do |email|
+      new_contact.add_email(email)
+    end
+
+    @contacts.push new_contact
   end
 end
