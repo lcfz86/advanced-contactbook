31a9e5f7579068adb87eb39c516896bf3f5c5718
lcfz86 <zerolcfz86@gmail.com>
2021-06-17 20:42:28 +0800

    Add Contact Book Part 4

--- contact_book_app.rb
+++ contact_book_app.rb
@@ -1,7 +1,10 @@
+# Contact Book Part 4
+require './contact_book'
+
 # Contact Book Part 1
 class ContactBookApp
   def initialize
-    @contact_book = nil
+    @contact_book = ContactBook.new
   end
 
   def run
@@ -15,5 +18,33 @@ class ContactBookApp
       print 'Invalid choice, please enter 1-4: '
       choice = get.chomp.to_i
     end
+
+    exit
+  end
+
+# Contact Book Part 4
+  def add_a_contact
+    print 'Enter a name: '
+    name = gets.chomp
+    phone_numbers = []
+    email = []
+
+    print 'Enter the phone number: '
+    phone_num = gets.chomp
+    phone_numbers.push phone_num
+    while phone_num != 'x'
+      print 'Enter additional phone number or type "x" to stop: '
+      phone_num = gets.chomp
+    end
+
+    print 'Enter the email address: '
+    email = gets.chomp
+    while email != 'x'
+      emails.push email
+      print 'Enter additional email address or type "x" to stop: '
+      email = gets.chomp
+    end
+
+    @contact_book.add_contact(name, phone_numbers, emails)
   end
 end
